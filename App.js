9e3ec84eb95cb4d7b939d9df10f2f5e6088ec1dd
Prakash Jagannath <prakashjs22@gmail.com>
2021-06-21 13:57:30 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import SplashScreen from './screens/Splash/SplashScreen';
+import Items from './screens/Items/Items';
+
+function App() {
+  return (
+    <>
+      <SplashScreen />
+      {/* <Items/> */}
+    </>
+  );
+}
+
+export default App;
